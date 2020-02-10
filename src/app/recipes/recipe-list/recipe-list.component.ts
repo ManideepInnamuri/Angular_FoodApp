33b1d7e737e41eb98dcf1171b59c1ef37c98d9e7
ManideepInnamuri <56725653+ManideepInnamuri@users.noreply.github.com>
2020-02-10 14:53:46 +0530

    Recipe Display on Select

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes:Recipe[]=[
-  new Recipe('Pizza', 'Pizza from Pizza Hut', 'https://res.cloudinary.com/swiggy/image/upload/f_auto,q_auto,fl_lossy/wmghlyfmhaj45fsq1jdc'),
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'Pizza from Pizza Hut', 'https://res.cloudinary.com/swiggy/image/upload/f_auto,q_auto,fl_lossy/wmghlyfmhaj45fsq1jdc'),
     new Recipe('Burger', 'Burger from MCDonalds', 'https://www.freepnglogos.com/uploads/burger-png/burger-png-png-images-yellow-images-12.png')
-];
+  ];
   constructor() { }
 
   ngOnInit() {
   }
-
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
